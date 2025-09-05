5c1dfefdad621bcf0f8f7903d7501d1c7a331821
bogdanzabotkin2002 <zabotkinbogdan2904@gmail.com>
2025-09-05 13:45:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 book = {"author": "Э.М. Ремарк", "title": "Три товарища",  "publication_year": 1936, "genre": "роман"}
 
 print(f"Название: {book['title']}, Автор: {book['author']}, Год:{book['publication_year']}, Жанр:{book['genre']}")
+
+# сложность алгоритма линейна О(N)
