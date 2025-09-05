0fe1a8356b1e20126b7cc4ec40179a20dd4a3683
bogdanzabotkin2002 <zabotkinbogdan2904@gmail.com>
2025-09-05 12:33:23 +0300

    zadanie_1

---
+++ main.py
@@ -0,0 +1,3 @@
+book = {"author": "Э.М. Ремарк", "title": "Три товарища",  "publication_year": 1936, "genre": "роман"}
+
+print(f"Название: {book['title']}, Автор: {book['author']}, Год:{book['publication_year']}, Жанр:{book['genre']}")
